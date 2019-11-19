fc2b0da5e566134fdbd2767c8e906d55ddf23c64
mabdess <bosch.abdessaied@extaccount.com>
2019-11-19 12:33:46 +0100

    first commit

---
+++ ocr_net.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+__author__ = "Mohamed Adnen Abdessaied"
+__maintainer__ = "Mohamed Adnen Abdessaied"
+__email__ = "adnenabdessayed@gmail.com"
+__status__ = "Implementation"
+
+
+import torch
+import
